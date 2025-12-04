048dcfd7ebf5e8ff96611a45c44704276dfb67e7
Lovi-Bot-NEW <Lena-Burdorf@web.de>
2025-12-04 15:57:02 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "arp-translator-bot",
+  "version": "1.0.0",
+  "description": "Discord Übersetzungs-Bot für ARP Allianz",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.15.2"
+  },
+  "type": "module"
+}
